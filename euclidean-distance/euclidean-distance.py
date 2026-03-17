9f9cbcdd7cfc9203288eedc793de5214650cb1a9
Oliver <n.hoangstarpie@gmail.com>
2026-03-17 12:36:29 +0700

    Add solution for euclidean-distance

---
+++ euclidean-distance/euclidean-distance.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def euclidean_distance(x, y):
+    """
+    Compute the Euclidean (L2) distance between vectors x and y.
+    Must return a float.
+    """
+    # Write code here
+    x = np.array(x)
+    y = np.array(y)
+
+    l2 = float(np.sqrt(np.sum((x-y)**2)))
+    
+    return l2
+    pass
