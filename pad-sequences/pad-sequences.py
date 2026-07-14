c462f1ced888a11fe65bb9dd23d3c6269915a7e7
Oliver <n.hoangstarpie@gmail.com>
2026-07-14 21:16:53 +0700

    Solve pad-sequences

--- pad-sequences/pad-sequences.py
+++ pad-sequences/pad-sequences.py
@@ -7,21 +7,26 @@ def pad_sequences(seqs, pad_value=0, max_len=None):
       L = max_len if provided else max(len(seq) for seq in seqs) or 0
     """
     # Your code here
-    
+    num_seqs = len(seqs)
     max_sent = max(len(seq) for seq in seqs)
-    
+
     if max_len is None:
         max_len = max_sent
-        
 
-    pad_sent = np.full((len(seqs), max_len), fill_value = pad_value)
+    pad_sent = np.full((num_seqs, max_len), fill_value = pad_value)
 
-    for i, sent in enumerate(seqs):
-        trunc_len = min(len(sent), max_len)
-        pad_sent[i, :trunc_len] = sent[:trunc_len]
+    lens = np.array([min(len(s), max_len) for s in seqs])
 
-    return pad_sent        
+    col_indices = np.arange(max_len)
 
-    
+    mask = col_indices < lens[:, None]
+
+    flat_vals = np.concatenate([np.asarray(s[:max_len]) for s in seqs])
+
+    pad_sent[mask] = flat_vals
+
+    return pad_sent
+
+
     
     pass
