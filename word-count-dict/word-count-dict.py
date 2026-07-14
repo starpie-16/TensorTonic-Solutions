fcd020e752fc1e8b5ab391512d930825b77bb74e
Oliver <n.hoangstarpie@gmail.com>
2026-07-14 23:00:43 +0700

    Solve word-count-dict

--- word-count-dict/word-count-dict.py
+++ word-count-dict/word-count-dict.py
@@ -7,9 +7,8 @@ def word_count_dict(sentences):
 
     for sent in sentences:
         for w in sent:
-            my_dict[w] = my_dict.get(w, 0) + 1
+            my_dict[w] = my_dict.get(w, 0)+1
 
     return my_dict
+        
     pass
-
-    
