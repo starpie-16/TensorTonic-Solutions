3c35aca08156406d21d7340fdb0a5a6b5fb49904
Oliver <n.hoangstarpie@gmail.com>
2026-07-16 23:23:43 +0700

    Solve tfidf-vectorizer

--- tfidf-vectorizer/tfidf-vectorizer.py
+++ tfidf-vectorizer/tfidf-vectorizer.py
@@ -8,25 +8,51 @@ def tfidf_vectorizer(documents):
     Returns tuple of (tfidf_matrix, vocabulary).
     """
     # Write code here
-    all_text = " ".join(documents)
-    all_words = all_text.split()
-    vocab = sorted(list(set(all_words)))
 
-    vocab = sorted(list(set(all_words)))
-    counts = np.array([[doc.split().count(word) for word in vocab] for doc in documents])    
+    
+    
+    words = [w for s in documents for w in s.split()]
+    vocab = np.unique(words)
+    
+    
 
-    #1 TF
-    row_sum = counts.sum(axis = 1, keepdims = True)
-    tf = counts / row_sum
+    #indexing
+    word_to_idx = {w: idx for idx, w in enumerate(vocab)}
+    indexed_docs = [[word_to_idx[w] for w in s.split()] for s in documents]
 
-    #2 IDF
-    df = (counts > 0).sum(axis = 0)
-    n = counts.shape[0]
-    idf = np.log(n/df)
+    #padding 
+    max_len = max(len(s) for s in indexed_docs)
+    padded_sent = np.full((len(indexed_docs), max_len), fill_value = -1) 
 
-    #3 TF-IDF
+    for i, sent in enumerate(indexed_docs):
+        padded_sent[i, :len(sent)] = sent
+
+
+
+    N = len(documents)
+    V = len(vocab)
+
+    
+
+    # tf
+    vocab_size = len(vocab)
+    vocab_indices = np.arange(V)
+
+    comparision = padded_sent[:, :, np.newaxis] == vocab_indices[np.newaxis, np.newaxis, :]
+    freq = np.sum(comparision, axis = 1)
+
+    row_sum = np.sum(freq, axis = 1)[:, None]
+    tf = freq / row_sum
+
+    # idf
+    df_bool = freq > 0
+    df = np.sum(df_bool, axis = 0)
+    idf = np.log(N / (df))
+
     tfidf_matrix = tf * idf
 
     return tfidf_matrix, vocab
+
+    
     
     pass
